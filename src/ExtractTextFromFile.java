519cf9398b6d9319c5267ee74b9125690b7bc658
ghelmer <ghelmer@d9e7442b-0272-427a-90c7-dca5c97caf01>
2011-09-05 02:12:52 +0000

    Return a String containing metadata information rather than printing directly to the System.out. Throw exceptions rather than handling them in the getText() method.
    git-svn-id: file:///Users/ghelmer/svn/projects/GradingHelper@10 d9e7442b-0272-427a-90c7-dca5c97caf01

--- src/ExtractTextFromFile.java
+++ src/ExtractTextFromFile.java
@@ -8,84 +8,71 @@ import java.io.File;
 import java.io.IOException;
 
 public class ExtractTextFromFile {
-	public void getText(String inputFn, String outputFn) {
-
-		/**
-		 * Check the command line
-		 */
-
-		File file = null;
-
-		file = new File(inputFn);
-
-		if (!file.exists() || !file.isFile()) {
-			System.out.println(file.toString() + " is not a valid file");
-			return;
+	public static String getText(File inputFile, File outputFile) throws IOException {
+		if (!inputFile.exists() || !inputFile.isFile()) {
+			throw new IllegalArgumentException(inputFile.toString() + " is not a valid file");
 		}
 
+		StringBuffer resultBuffer = new StringBuffer();
+		
 		/**
 		 * Create and setup the SecureRequest object
 		 */
 		SecureRequest request = new SecureRequest();
 		/* ScrubbedDocument: FileOption */
 		//File scrubbedFile = new File("/tmp/output.bin");
-		File outputFile = new File(outputFn);
 		request.setOption(SecureOptions.JustAnalyze, true);
 		//request.setOption(SecureOptions.ScrubbedDocument, scrubbedFile);
-		request.setOption(SecureOptions.SourceDocument, file);
+		request.setOption(SecureOptions.SourceDocument, inputFile);
 		request.setOption(SecureOptions.ResultDocument, outputFile);
 		request.setOption(SecureOptions.OutputType, SecureOptions.OutputTypeOption.ToText);
 		request.setOption(SecureOptions.ToTextEncoding, SecureOptions.ToTextEncodingOption.UTF8);
-		try	{
+
+		/**
+		 * Execute the request
+		 */
+
+		request.execute();
+
+		/**
+		 * Get the response
+		 */
+
+		SecureResponse response = request.getResponse();
+
+		if (response.getResult(SecureOptions.WasProcessed)) {
 			/**
-			 * Execute the request
+			 * Prints all the scrub and analyze targets in the file.
+			 * Note that in a production version a developer would probably
+			 * want to cache the list scrub/analyse targets to improve
+			 * performance.
 			 */
-
-			request.execute();
-
-			/**
-			 * Get the response
-			 */
-
-			SecureResponse response = request.getResponse();
-
-			if (response.getResult(SecureOptions.WasProcessed)) {
-				/**
-				 * Prints all the scrub and analyze targets in the file.
-				 * Note that in a production version a developer would probably
-				 * want to cache the list scrub/analyse targets to improve
-				 * performance.
-				 */
-				System.out.println("The file " + file.getName() +
-						" of format " +
-						response.getResult(SecureOptions.SourceFormat).getName() +
-						" contains:");
-				Option[] options = SecureOptions.getInstance().getAllOptions();
-				for (int j = 0; j < options.length; j++) {
-					if (options[j] instanceof ScrubOption) {
-						if (response.getResult((ScrubOption) options[j]) == ScrubOption.Reaction.EXISTS)
-							System.out.println("   " + options[j].getName());
-					} else if (options[j] instanceof AnalyzeOption) {
-						if (response.getResult((AnalyzeOption) options[j]) == AnalyzeOption.Reaction.EXISTS)
-							System.out.println("   " + options[j].getName());
+			resultBuffer.append("The file " + inputFile.getName() +
+					" of format " +
+					response.getResult(SecureOptions.SourceFormat).getName() +
+					" contains:\n");
+			Option[] options = SecureOptions.getInstance().getAllOptions();
+			for (int j = 0; j < options.length; j++) {
+				if (options[j] instanceof ScrubOption) {
+					if (response.getResult((ScrubOption) options[j]) == ScrubOption.Reaction.EXISTS)
+					{
+						resultBuffer.append('\t');
+						resultBuffer.append(options[j].getName());
+						resultBuffer.append('\n');
+					}
+				} else if (options[j] instanceof AnalyzeOption) {
+					if (response.getResult((AnalyzeOption) options[j]) == AnalyzeOption.Reaction.EXISTS)
+					{
+						resultBuffer.append('\t');
+						resultBuffer.append(options[j].getName());
+						resultBuffer.append('\n');
 					}
 				}
-			} else {
-				System.out.println("The file " + file.getName() + " of format " + response.getResult(SecureOptions.SourceFormat).getName() + " could not be processed.");
 			}
-		} catch (IOException e) {
-			System.out.println("IOException in file " + file.getName());
-			e.printStackTrace();
-		} catch (RuntimeException e) {
-			System.out.println("RuntimeException in file " + file.getName());
-			e.printStackTrace();
-		} catch (OutOfMemoryError e) {
-			System.out.println("OutOfMemoryError in file " + file.getName());
-			e.printStackTrace();
-		} catch (StackOverflowError e) {
-			System.out.println("StackOverflowError in file " + file.getName());
-			e.printStackTrace();
+		} else {
+			resultBuffer.append("The file " + inputFile.getName() + " of format " + response.getResult(SecureOptions.SourceFormat).getName() + " could not be processed.\n");
 		}
+		return resultBuffer.toString();
 	}
 }
 
