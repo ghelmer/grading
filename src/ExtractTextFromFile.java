aff2879390f605b044defeaf34ed00db43afb341
ghelmer <ghelmer@d9e7442b-0272-427a-90c7-dca5c97caf01>
2013-02-12 02:14:01 +0000

    Replace use of Oracle CleanContent with LibreOffice 3.6 + unoconv 0.6.
    git-svn-id: file:///Users/ghelmer/svn/projects/GradingHelper@26 d9e7442b-0272-427a-90c7-dca5c97caf01

--- src/ExtractTextFromFile.java
+++ src/ExtractTextFromFile.java
@@ -1,78 +1,44 @@
-import net.bitform.api.options.AnalyzeOption;
-import net.bitform.api.options.Option;
-import net.bitform.api.options.ScrubOption;
-import net.bitform.api.secure.SecureOptions;
-import net.bitform.api.secure.SecureRequest;
-import net.bitform.api.secure.SecureResponse;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class ExtractTextFromFile {
 	public static String getText(File inputFile, File outputFile) throws IOException {
 		if (!inputFile.exists() || !inputFile.isFile()) {
 			throw new IllegalArgumentException(inputFile.toString() + " is not a valid file");
 		}
-
-		StringBuffer resultBuffer = new StringBuffer();
 		
-		/**
-		 * Create and setup the SecureRequest object
-		 */
-		SecureRequest request = new SecureRequest();
-		/* ScrubbedDocument: FileOption */
-		//File scrubbedFile = new File("/tmp/output.bin");
-		request.setOption(SecureOptions.JustAnalyze, true);
-		//request.setOption(SecureOptions.ScrubbedDocument, scrubbedFile);
-		request.setOption(SecureOptions.SourceDocument, inputFile);
-		request.setOption(SecureOptions.ResultDocument, outputFile);
-		request.setOption(SecureOptions.OutputType, SecureOptions.OutputTypeOption.ToText);
-		request.setOption(SecureOptions.ToTextEncoding, SecureOptions.ToTextEncodingOption.UTF8);
-
-		/**
-		 * Execute the request
-		 */
-
-		request.execute();
-
-		/**
-		 * Get the response
-		 */
-
-		SecureResponse response = request.getResponse();
-
-		if (response.getResult(SecureOptions.WasProcessed)) {
-			/**
-			 * Prints all the scrub and analyze targets in the file.
-			 * Note that in a production version a developer would probably
-			 * want to cache the list scrub/analyse targets to improve
-			 * performance.
-			 */
-			resultBuffer.append("The file " + inputFile.getName() +
-					" of format " +
-					response.getResult(SecureOptions.SourceFormat).getName() +
-					" contains:\n");
-			Option[] options = SecureOptions.getInstance().getAllOptions();
-			for (int j = 0; j < options.length; j++) {
-				if (options[j] instanceof ScrubOption) {
-					if (response.getResult((ScrubOption) options[j]) == ScrubOption.Reaction.EXISTS)
-					{
-						resultBuffer.append('\t');
-						resultBuffer.append(options[j].getName());
-						resultBuffer.append('\n');
-					}
-				} else if (options[j] instanceof AnalyzeOption) {
-					if (response.getResult((AnalyzeOption) options[j]) == AnalyzeOption.Reaction.EXISTS)
-					{
-						resultBuffer.append('\t');
-						resultBuffer.append(options[j].getName());
-						resultBuffer.append('\n');
-					}
-				}
-			}
-		} else {
-			resultBuffer.append("The file " + inputFile.getName() + " of format " + response.getResult(SecureOptions.SourceFormat).getName() + " could not be processed.\n");
+		// Use LibreOffice + unoconv to convert the document
+		// /Applications/LibreOffice.app/Contents/MacOS/python ~/Downloads/unoconv-0.6/unoconv --format=text --output=testing.txt ~/Documents/testing.odt
+		String args[] = new String[5];
+		args[0] = "/Applications/LibreOffice.app/Contents/MacOS/python";
+		args[1] = "/Users/ghelmer/Downloads/unoconv-0.6/unoconv";
+		args[2] = "--format=text";
+		args[3] = "--output=" + outputFile.getAbsolutePath();
+		args[4] = inputFile.getAbsolutePath();
+		Runtime r = Runtime.getRuntime();
+		Process result = r.exec(args);
+		Scanner in = new Scanner(result.getErrorStream());
+		StringBuffer output = new StringBuffer();
+		while (in.hasNextLine())
+		{
+			output.append(in.nextLine() + "\n");
 		}
-		return resultBuffer.toString();
+		try {
+			result.waitFor();
+			if (output.length() != 0)
+			{
+				System.out.println("Output form unoconv " + inputFile.getAbsolutePath() + " to " + outputFile.getAbsolutePath() + ":");
+				System.out.println(output);
+			}
+			else if (result.exitValue() != 0)
+			{
+				System.out.println("unoconv exit code: " + result.exitValue());
+			}
+		} catch (InterruptedException e) {
+			System.err.println("unoconv Interrupted Exception: " + e.getMessage());
+		}
+		return "The file " + inputFile.getName() + " contains:\n";
 	}
 }
 
